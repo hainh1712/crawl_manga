c02682c8b279d962175ced25f152a47278a96a88
hainh1712 <nguyenhoanghai20021712@gmail.com>
2024-01-16 14:27:49 +0700

    push image to s3

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
+import os
+import csv
+import re
+import time
+import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-import time
-import re
-import csv
-import os
-import requests
+import boto3
+from botocore.exceptions import NoCredentialsError
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+
+s3 = boto3.client("s3", aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'], aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'], region_name=os.environ['AWS_REGION'])
+def upload_to_aws(local_file, s3_file):
+    s3 = boto3.client('s3', aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'], aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
+    try:
+        s3.upload_file(local_file, os.environ['AWS_BUCKET_NAME'], s3_file, ExtraArgs={
+            'ContentType': 'jpg',
+        })
+        print(f"Upload successful: {s3_file}")
+        return True
+    except NoCredentialsError:
+        print("Credentials not available")
+        return False
+# Configure Chrome options
 chrome_options = ChromeOptions()
+# Run Chrome in headless mode (no GUI)
 chrome_options.add_argument("--headless=new")
-chrome_options = ChromeOptions()
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
 chrome_options.add_argument(f'user-agent={user_agent}')
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
@@ -53,6 +73,10 @@ def get_chapter_link():
         driver.quit()
 
 def get_image(url, chapter_name):
+    pattern = r"\d+"
+    result = re.search(pattern, chapter_name)
+    chapter = result.group()
+    
     folder_path = f"conan/{chapter_name}"
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
@@ -68,18 +92,19 @@ def get_image(url, chapter_name):
 
         with open(img_path, "wb") as img_file:
             img_file.write(img_data)
-
-        print(f"Image {index} saved at {img_path}")
+            
+        s3_file_path = f"conan/chap_{chapter}/{index}.jpg"
+        upload_to_aws(img_path, s3_file_path)
+        # print(f"Image {index} saved at {img_path}")
+        print(f"Image {index} uploaded to S3 at {s3_file_path}")
 
 
 def main():
-    with open(os.path.join(folder_path, "chapter_links.csv"), "r", newline="", encoding="utf-8") as f:
+    with open(os.path.join(folder_path, "chapter_link.csv"), "r", newline="", encoding="utf-8") as f:
         reader = csv.reader(f)
         next(reader)
         for row in reader:
             get_image(row[1], row[0])
 
 main()
-
-
 
