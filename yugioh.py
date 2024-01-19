704ff8114155d8b60fe05b4346c9522ab56d639b
hainh1712 <nguyenhoanghai20021712@gmail.com>
2024-01-19 12:28:21 +0700

    update get image

--- yugioh.py
+++ yugioh.py
@@ -9,22 +9,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 import boto3
 from botocore.exceptions import NoCredentialsError
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# load_dotenv('.env')
+load_dotenv('.env')
 
-# s3 = boto3.client("s3", aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'], aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'], region_name=os.environ['AWS_REGION'])
-# def upload_to_aws(local_file, s3_file):
-#     s3 = boto3.client('s3', aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'], aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
-#     try:
-#         s3.upload_file(local_file, os.environ['AWS_BUCKET_NAME'], s3_file, ExtraArgs={
-#             'ContentType': 'jpg',
-#         })
-#         print(f"Upload successful: {s3_file}")
-#         return True
-#     except NoCredentialsError:
-#         print("Credentials not available")
-#         return False
+s3 = boto3.client("s3", aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'], aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'], region_name=os.environ['AWS_REGION'])
+def upload_to_aws(local_file, s3_file):
+    s3 = boto3.client('s3', aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'], aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
+    try:
+        s3.upload_file(local_file, os.environ['AWS_BUCKET_NAME'], s3_file, ExtraArgs={
+            'ContentType': 'jpg',
+        })
+        print(f"Upload successful: {s3_file}")
+        return True
+    except NoCredentialsError:
+        print("Credentials not available")
+        return False
 # Configure Chrome options
 chrome_options = ChromeOptions()
 # Run Chrome in headless mode (no GUI)
@@ -39,9 +39,9 @@ folder_path = "yugioh"
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
 
-with open(os.path.join(folder_path, "chapter_links.csv"), "w", newline="", encoding="utf-8") as f:
-    writer = csv.writer(f)
-    writer.writerow(["chapter", "link"])
+# with open(os.path.join(folder_path, "chapter_links.csv"), "w", newline="", encoding="utf-8") as f:
+#     writer = csv.writer(f)
+#     writer.writerow(["chapter", "link"])
 
 def modified_chapter_name(chapter_name):
    title_pattern = r"(YuGi-Oh!)"
@@ -75,40 +75,40 @@ def get_chapter_link():
         with open(os.path.join(folder_path, "chapter_links.csv"), "a", newline="", encoding="utf-8") as f:
             writer = csv.writer(f)
             writer.writerow([chapter_name, href])
+
 get_chapter_link()
-# def get_image(url, chapter_name):
-#     pattern = r"\d+"
-#     result = re.search(pattern, chapter_name)
-#     chapter = result.group()
-    
-#     folder_path = f"conan/{chapter_name}"
-#     if not os.path.exists(folder_path):
-#         os.makedirs(folder_path)
-#     driver = webdriver.Chrome(service=ChromeService("./chromedriver.exe"), options=chrome_options)
-#     driver.get(url)
-#     time.sleep(2)
-#     wrapper = driver.find_element(By.CLASS_NAME, "bbWrapper")
-#     images = wrapper.find_elements(By.CLASS_NAME, "bbImage")
-#     for index, image in enumerate(images, start=1):
-#         img_src = image.get_attribute("src")
-#         img_data = requests.get(img_src).content
-#         img_path = os.path.join(folder_path, f"{index}.jpg")
+def get_image(url, chapter_name):
+    chapter = chapter_name.split(" ")[-1]
+    print(chapter)
+    folder_path = f"yugioh/{chapter_name}"
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    driver = webdriver.Chrome(service=ChromeService("./chromedriver.exe"), options=chrome_options)
+    driver.get(url)
+    time.sleep(2)
+    list_image = driver.find_element(By.TAG_NAME, "article")
+    images = list_image.find_elements(By.TAG_NAME, "img")
+    for index in range(1, len(images)-1):
+        image = images[index]
+        img_src = image.get_attribute("src")
+        img_data = requests.get(img_src).content
+        img_path = os.path.join(folder_path, f"{index}.jpg")
 
-#         with open(img_path, "wb") as img_file:
-#             img_file.write(img_data)
+        with open(img_path, "wb") as img_file:
+            img_file.write(img_data)
             
-#         s3_file_path = f"conan/chap_{chapter}/{index}.jpg"
-#         upload_to_aws(img_path, s3_file_path)
-#         # print(f"Image {index} saved at {img_path}")
-#         print(f"Image {index} uploaded to S3 at {s3_file_path}")
+        # s3_file_path = f"yugioh/chap_{chapter}/{index}.jpg"
+        # upload_to_aws(img_path, s3_file_path)
+        # print(f"Image {index} saved at {img_path}")
+        # print(f"Image {index} uploaded to S3 at {s3_file_path}")
 
 
-# def main():
-#     with open(os.path.join(folder_path, "chapter_link.csv"), "r", newline="", encoding="utf-8") as f:
-#         reader = csv.reader(f)
-#         next(reader)
-#         for row in reader:
-#             get_image(row[1], row[0])
+def main():
+    with open(os.path.join(folder_path, "chapter_links_test.csv"), "r", newline="", encoding="utf-8") as f:
+        reader = csv.reader(f)
+        next(reader)
+        for row in reader:
+            get_image(row[1], row[0])
 
-# main()
+main()
 
